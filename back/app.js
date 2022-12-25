ae6278aa4bf8ffd687aa98eb2d8f72fff09beed8
waykutwoo <aykutkokluk@gmail.com>
2022-12-25 20:38:16 +0300

    fonction delete du backend terminé
    ready

--- back/app.js
+++ back/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-
+const path = require('path');
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
 
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
 
--- back/routes/stuff.js
+++ back/routes/stuff.js
@@ -4,10 +4,12 @@ const router = express.Router();
 
 const auth = require('../middleware/auth');
 
+const multer = require('../middleware/multer-config');
+
 const stuffCtrl = require('../controllers/stuff');
 
-router.post('/', auth, stuffCtrl.createThing);
-router.put('/:id',auth, stuffCtrl.modifyThing);
+router.post('/', auth, multer, stuffCtrl.createThing);
+router.put('/:id',auth, multer, stuffCtrl.modifyThing);
 router.delete('/:id', auth, stuffCtrl.deleteThing);
 router.get('/:id', auth, stuffCtrl.getOneThing);
 router.get('/', auth, stuffCtrl.getAllThings);
